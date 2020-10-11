ed3e1e00ad4deb64140ba592a1da6c50f250b324
navada-me <permindersingh93@gmail.com>
2020-10-11 16:18:55 -0400

    db config

--- notes/settings.py
+++ notes/settings.py
@@ -69,12 +69,10 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
