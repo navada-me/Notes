55ad0a7b4c912acc7ed573ee77035e69ca08da83
navada-me <permindersingh93@gmail.com>
2020-10-15 14:38:43 -0400

    secret key removed

--- notes/settings.py
+++ notes/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jaf5t6h&rso6#)hvjtlt6-o-okw68^eqjipt$n-4w%&6ihfzl1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
