92b492c62db4d55a0568ff66b42e1d84357a2ab9
navada-me <permindersingh93@gmail.com>
2020-10-16 19:51:58 -0400

    hiding secret key

--- notes/settings.py
+++ notes/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -71,24 +71,24 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'todo',
-            'USER': 'kali',
-            'PASSWORD': 'kali',
-            'HOST': 'localhost',
-            'PORT': '',
-    }
-}
-
-
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
+#     'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': 'todo',
+#             'USER': 'kali',
+#             'PASSWORD': 'kali',
+#             'HOST': 'localhost',
+#             'PORT': '',
+#     }
 # }
+
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 
